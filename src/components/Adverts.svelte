07ac0b85dfe5932bdaa26f4c7bd72345b16ed67b
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-21 16:43:59 +0000

    Remove CSS class on Adverts component

--- src/components/Adverts.svelte
+++ src/components/Adverts.svelte
@@ -1,12 +1,12 @@
-<section class="adverts">
-    <p>Hello world</p>
-  </section>
-  
-  <style>
-    .adverts {
-      max-width: 90vw;
-      border: 1px solid white;
-      margin: auto;
-      height: 300px;
-    }
-  </style>
+<section>
+  <p>Hello world</p>
+</section>
+
+<style>
+  section {
+    max-width: 90vw;
+    border: 1px solid white;
+    margin: auto;
+    height: 300px;
+  }
+</style>
