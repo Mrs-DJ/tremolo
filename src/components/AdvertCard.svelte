1b657988d5ea4e78a479529a8c44951c4669f1ca
Damog83 <damiengittings@hotmail.co.uk>
2022-03-22 11:58:53 +0000

    article component added, displayed within a grid

---
+++ src/components/AdvertCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let group;
+    export let location;
+    export let role;
+</script>
+
+<div>
+    <h2>{group}</h2>
+    <p>{location}</p>
+    <p>{role}</p>
+</div>
--- src/components/Adverts.svelte
+++ src/components/Adverts.svelte
@@ -1,18 +1,22 @@
 <script>
-  import { adverts } from "../../__testdata.json";
-
-  console.log(adverts);
+	import { adverts } from "../../__testdata.json";
+	import AdvertCard from "./AdvertCard.svelte";
 </script>
 
 <section>
-  <p>Hello world</p>
+	{#each adverts as {group, location, role}}
+	<AdvertCard {group} {location} {role}/>
+
+	{/each}
 </section>
 
 <style>
-  section {
-    max-width: 90vw;
-    border: 1px solid white;
-    margin: auto;
-    height: 300px;
-  }
+	section {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));   
+		grid-gap: 10px;   
+	    padding: 5px;
+		max-width: 90vw;
+		margin: auto;
+	}
 </style>
