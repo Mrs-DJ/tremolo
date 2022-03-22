646150a7a8617f5b7575a3387edcb3397b03c3c7
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-22 13:00:38 +0000

    Add firestore query

--- src/components/Adverts.svelte
+++ src/components/Adverts.svelte
@@ -1,22 +1,27 @@
 <script>
-	import { adverts } from "../../__testdata.json";
-	import AdvertCard from "./AdvertCard.svelte";
+  import { collection, getDocs } from "firebase/firestore";
+  import { adverts } from "../../__testdata.json";
+  import { db } from "../firebase";
+  import AdvertCard from "./AdvertCard.svelte";
 </script>
 
 <section>
-	{#each adverts as {group, location, role}}
-	<AdvertCard {group} {location} {role}/>
-
-	{/each}
+  {#await getDocs(collection(db, "Adverts")) then results}
+    {console.log(results)}
+    
+  {/await}
+  {#each adverts as { group, location, role }}
+    <AdvertCard {group} {location} {role} />
+  {/each}
 </section>
 
 <style>
-	section {
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));   
-		grid-gap: 10px;   
-	    padding: 5px;
-		max-width: 90vw;
-		margin: auto;
-	}
+  section {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 10px;
+    padding: 5px;
+    max-width: 90vw;
+    margin: auto;
+  }
 </style>
