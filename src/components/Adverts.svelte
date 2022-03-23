a1c5cc84b9d908436c22c6a5aa1260033ef964a5
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-23 12:17:06 +0000

    Add dropdown with on change function

--- src/components/Adverts.svelte
+++ src/components/Adverts.svelte
@@ -1,5 +1,8 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
+</svelte:head>
+
 <script>
-  import Select, { Option } from "@smui/select";
   import { collection, getDocs } from "firebase/firestore";
   import { db } from "../firebase";
   import AdvertCard from "./AdvertCard.svelte";
@@ -13,31 +16,75 @@
     adverts = adverts;
   });
 
-  const genres = ["Indie", "Alternative", "Jazz", "Blues", "Folk", "Pop", "Shoegaze", "Dream-pop", "Hardcore", "Punk", "Rock"];
-  let value;
+  const genres = [
+    "Indie",
+    "Alternative",
+    "Jazz",
+    "Blues",
+    "Folk",
+    "Pop",
+    "Shoegaze",
+    "Dream-pop",
+    "Hardcore",
+    "Punk",
+    "Rock",
+  ];
+
+  $: filteredAdverts = adverts;
+
+  const setGenre = ({ target: { value } }) => {
+    if (value === "All") {
+      filteredAdverts = filteredAdverts;
+    } else {
+      filteredAdverts = adverts.filter((ad) => ad.genre.includes(value));
+    }
+  };
 </script>
 
 <section>
-	<div>
-		<Select bind:value>
-			{#each genres as genre}
-			  	<Option value={genre}>{genre}</Option>
-			{/each}
-		</Select>
-		<p>Selected:{value}</p>
-	</div>
-  {#each adverts as advert}
-    <AdvertCard {...advert} />
-  {/each}
+  <div class="genre-filter">
+    <i class="mi mi-filter-alt"></i>
+    <select class="dropdown"on:change={setGenre}>
+      <option>All</option>
+      {#each genres as genre}
+        <option value={genre}>{genre}</option>
+      {/each}
+    </select>
+  </div>
+  <section class="advert-grid">
+    {#each filteredAdverts as { advert_title: title, band_name: group, body, instrument_required: instruments }}
+      <AdvertCard {title} {group} {instruments} {body} />
+    {/each}
+  </section>
 </section>
 
 <style>
-  section {
+  .advert-grid {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     grid-gap: 10px;
-    padding: 5px;
-    max-width: 90vw;
-    margin: auto;
   }
+
+  section {
+    padding: 5px;
+    margin: auto;
+    max-width: 90vw;
+  }
+
+  p {
+    margin-right: 5px;
+  }
+
+  .genre-filter {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .dropdown {
+    height: 2.2rem;
+  }
+
+  .mi {
+		font-size: 2rem;
+	}
 </style>
