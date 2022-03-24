0b7a082d5511e1ae15c7516dbbf48396d01017e0
Matt J Hampson <mjhampson@gmail.com>
2022-03-24 14:57:49 +0000

    advert page work in progress

---
+++ src/routes/advert.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let id;
+  export let title;
+  export let group;
+  export let instruments;
+  export let body;
+  import { collection, getDoc } from "firebase/firestore";
+  import { db } from "../firebase";
+  //   import AdvertCard from "./AdvertCard.svelte";
+
+  let advert = [];
+
+  const adRef = collection(db, "advert");
+  getDoc(adRef).then((results) => {
+    results.forEach((doc) => {
+      advert.push(doc.data());
+    });
+    advert = advert;
+  });
+</script>
+
+<div>
+  <h2 class="text-xl font-bold">{title}</h2>
+  <p class="text-base text-m">{group}</p>
+  <p class="text-xs">{instruments}</p>
+  <p class="text-xs">{body}</p>
+</div>
