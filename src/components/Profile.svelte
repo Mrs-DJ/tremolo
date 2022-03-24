d1ef8a25ebd820ded95d993de6c23cefd4dc35a5
tubez7 <thescopeboys@hotmail.com>
2022-03-24 16:15:25 +0000

    update profile

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -6,11 +6,12 @@
 
   let profile = {};
   let instruments = [];
-  $: value = profile.bio;
+  $: value = profile.bio || "";
 
   const colRef = doc(db, "Users", uid);
   getDoc(colRef).then((result) => {
     profile = result.data();
+    console.log(profile);
     profile.instrument.forEach((instrument) => {
       instruments.push(instrument);
     });
@@ -40,6 +41,22 @@
   <h1>{auth.currentUser.displayName}</h1>
   <form on:submit={setUser}>
     <textarea {value} on:change={setValue} />
+    <span class="checkbox-flex">
+    <label for="guitar">Guitar</label>
+    <input type="checkbox" name="guitar" value="guitar" checked={instruments.includes(value.toLowerCase())}/>
+    <label for="bass">Bass</label>
+    <input type="checkbox" name="bass" value="bass" />
+    <label for="drums">Drums</label>
+    <input type="checkbox" name="drums" value="drums" />
+    <label for="vocals">Vocals</label>
+    <input type="checkbox" name="vocals" value="vocals" />
+    <label for="keys">Keyboards/Synth</label>
+    <input type="checkbox" name="keys" value="keys" />
+    <label for="other">Other</label>
+    <input type="checkbox" name="other" value="other" />
+    </span>
+
+
     <button type="submit">Update</button>
   </form>
 </section>
@@ -54,5 +71,12 @@
   }
   h1 {
     font-size: 40px;
+  }
+  .checkbox-flex {
+    display: flex;
+
+  }
+  label, input {
+    margin: 3px;
   }
 </style>
