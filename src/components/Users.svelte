be087b2f746fc43227ba763a71799352c1c070cd
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-03-24 11:30:33 +0000

    Added a filter by instrument on user page

--- src/components/Users.svelte
+++ src/components/Users.svelte
@@ -3,8 +3,10 @@
     import { db } from "../firebase";
     import UserCard from "./UserCard.svelte";
 
-    const colRef = collection(db, "Users");
     let users = [];
+    $: filteredUsers = users;
+    
+    const colRef = collection(db, "Users");
 
     getDocs(colRef).then((results) => {
       results.forEach((doc) => {
@@ -12,15 +14,59 @@
       });
       users = users;
     });
+
+    const instruments = [
+      "Piano",
+      "Saxophone",
+      "Guitar",
+      "Xylophone",
+      "Panflute",
+      "MC",
+      "Drums",
+      "Bass guitar",
+      "Harp",
+      "Triangle",
+      "Vocals",
+      "Producer",
+      "Multi-instrumentalist",
+      "Flute",
+      "Banjo",
+      "Ukelele",
+    ];
+
+const setInstrument = ({ target: { value } }) => {
+      if (value === "All") {
+        filteredUsers = filteredUsers;
+      } else {
+        filteredUsers = users.filter((user) => user.instrument.includes(value));
+  }
+};
 </script>
   
-  <section>
-    {#each users as { username, name, location, bio, instrument }}
+<section>
+    <div class="instrument-filter">
+      <i class="mi mi-filter-alt" />
+      <select class="dropdown" on:change={setInstrument}>
+        <option>All</option>
+        {#each instruments as instrument}
+          <option value={instrument}>{instrument}</option>
+        {/each}
+      </select>
+    </div>
+    <section class="user-grid">
+    {#each filteredUsers as { username, name, location, bio, instrument }}
       <UserCard {username} {name} {location} {instrument} {bio} />
     {/each}
   </section>
+</section>
   
   <style>
+
+    .user-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 10px;
+  }
     section {
       display: grid;
       grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
@@ -29,4 +75,9 @@
       max-width: 90vw;
       margin: auto;
     }
+
+    .dropdown {
+    height: 2.2rem;
+    background-color: #333;
+  }
   </style>
