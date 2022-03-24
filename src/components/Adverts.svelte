035f75fef9de6cacd3d3ed239b48eb38dceb5a74
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-03-24 09:51:37 +0000

    Merge conflict branch

--- src/components/Adverts.svelte
+++ src/components/Adverts.svelte
@@ -1,46 +1,65 @@
 <script>
-  import { collection, getDocs } from "firebase/firestore";
-  import { db } from "../firebase";
-  import AdvertCard from "./AdvertCard.svelte";
-  
-  let adverts = [];
-  $: filteredAdverts = adverts;
-
-  const colRef = collection(db, "Adverts");
-  getDocs(colRef).then((results) => {
-    results.forEach((doc) => {
-      adverts.push(doc.data());
+    import { collection, getDocs } from "firebase/firestore";
+    import { db } from "../firebase";
+    import AdvertCard from "./AdvertCard.svelte";
+    
+    let adverts = [];
+    $: filteredAdverts = adverts;
+    
+    const colRef = collection(db, "Adverts");
+    getDocs(colRef).then((results) => {
+      results.forEach((doc) => {
+        adverts.push(doc.data());
+      });
+      adverts = adverts;
     });
-    adverts = adverts;
-  });
+    
+    const genres = [
+      "Indie",
+      "Alternative",
+      "Jazz",
+      "Blues",
+      "Folk",
+      "Pop",
+      "Shoegaze",
+      "Dream-pop",
+      "Hardcore",
+      "Punk",
+      "Rock",
+    ];
 
-  const genres = [
-    "Indie",
-    "Alternative",
-    "Jazz",
-    "Blues",
-    "Folk",
-    "Pop",
-    "Shoegaze",
-    "Dream-pop",
-    "Hardcore",
-    "Punk",
-    "Rock",
-  ];
-
-  const setGenre = ({ target: { value } }) => {
-    if (value === "All") {
-      filteredAdverts = filteredAdverts;
-    } else {
-      filteredAdverts = adverts.filter((ad) => ad.genre.includes(value));
-    }
-  };
+    const setGenre = ({ target: { value } }) => {
+      if (value === "All") {
+        filteredAdverts = filteredAdverts;
+      } else {
+        filteredAdverts = adverts.filter((ad) => ad.genre.includes(value));
+      }
+    };
 </script>
 
-<section class="grid-template-columns: repeat(1, minmax(0, 1fr))">
-  {#each adverts as { advert_title: title, band_name: group, body, instrument_required: instruments }}
-    <AdvertCard {title} {group} {instruments} {body} />
-  {/each}
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css"
+  />
+</svelte:head>
+
+<section>
+  <div class="genre-filter">
+    <i class="mi mi-filter-alt" />
+    <select class="dropdown" on:change={setGenre}>
+      <option>All</option>
+      {#each genres as genre}
+        <option value={genre}>{genre}</option>
+      {/each}
+    </select>
+  </div>
+  <section class="advert-grid">
+    {#each adverts as { advert_title: title, band_name: group, body, instrument_required: instruments }}
+      <AdvertCard {title} {group} {instruments} {body} />
+    {/each}
+  </section>
 </section>
 
 <style>
@@ -56,9 +75,9 @@
     max-width: 90vw;
   }
 
-  p {
+  /* p {
     margin-right: 5px;
-  }
+  } */
 
   .genre-filter {
     display: flex;
