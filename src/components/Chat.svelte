5afaca8226b35bdabb3195e0bc622182ebb39e40
tubez7 <thescopeboys@hotmail.com>
2022-03-28 13:34:03 +0100

    update progress on messages

--- src/components/Chat.svelte
+++ src/components/Chat.svelte
@@ -1,5 +1,33 @@
 <script>
+    import {db, auth} from "../firebase.js";
+    import {collection, addDoc, query, orderBy, limit, onSnapshot, setDoc, updateDoc, doc, serverTimestamp} from "firebase/firestore";
     export let id;
+    let uid;
+    let message;
+
+
+
+    const setMessage = (e) => {
+        message = e.target.value;
+    };
+
+
+    // auth.operations.then(() => {
+    // uid = auth.currentUser.uid;}
+
+    const addMessage = (e) => {
+        e.preventDefault();
+        addDoc(doc(db, "Chats"),
+        {
+            text: message,
+            timestamp: serverTimestamp(),
+        }
+    }
 </script>
 
 <p>{id}</p>
+
+<form on:submit={addMessage}>
+<input type="text" on:change={setMessage}/>
+<button type="submit">Send Message</button>
+</form>
